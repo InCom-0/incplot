91c050d729e42236a359623e284ad3f9f9a945f8
Michal Lurie <Michal.Lurie@gmail.com>
2025-05-20 13:17:04 +0200

    Using new simplified library interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <print>
-#include <string_view>
 #include <unistd.h>
 
 #include <incplot.hpp>
@@ -24,38 +23,9 @@ int main(int argc, char *argv[]) {
 
 
     std::string const input((std::istreambuf_iterator(std::cin)), std::istreambuf_iterator<char>());
-    auto              ds = incplot::parsers::Parser::parse(std::string_view(input));
-
 
     for (auto const &dpctr : incplot::CL_Args::get_dpCtorStruct(ap, argc, argv)) {
-        auto dp_autoGuessed = incplot::DesiredPlot(dpctr).guess_missingParams(ds.value());
-
-
-        if (not dp_autoGuessed.has_value()) {
-            std::print("{0}{1}{2}", "Autoguessing of 'DesiresPlot' parameters for: ",
-                       dpctr.plot_type_name.has_value() ? dpctr.plot_type_name.value() : "[Unspecified plot type]",
-                       " failed.\n");
-            continue;
-        }
-
-        auto plotDrawer = incplot::make_plotDrawer(dp_autoGuessed.value(), ds.value());
-        if (not plotDrawer.has_value()) {
-            std::print("{0}{1}{2}", "Creating 'Plot Structure' for: ",
-                       dpctr.plot_type_name.has_value() ? dpctr.plot_type_name.value() : "[Unspecified plot type]",
-                       " failed.\n");
-            continue;
-        }
-
-
-        auto outExp = plotDrawer.value().validateAndDrawPlot();
-
-        if (not outExp.has_value()) {
-            std::print("{0}{1}{2}", "Invalid plot structure for: ",
-                       dpctr.plot_type_name.has_value() ? dpctr.plot_type_name.value() : "[Unspecified plot type]",
-                       ".");
-            continue;
-        }
-        std::print("{}\n", outExp.value());
+        std::print("{}\n", incplot::make_plot_collapseUnExp(dpctr, input));
     }
 
     return 0;
