286d984d959a91817f86d7e746e63069e4127e36
Michal Lurie <Michal.Lurie@gmail.com>
2025-05-24 17:06:32 +0200

    Minor cleanup

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,13 +1,5 @@
 include(FetchContent)
 
-# FetchContent_Declare(
-#     googletest
-#     GIT_REPOSITORY https://github.com/google/googletest.git
-#     GIT_TAG v1.16.0
-# )
-# set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-# FetchContent_MakeAvailable(googletest)
-
 FetchContent_Declare(
     incplot-lib
     GIT_REPOSITORY https://github.com/InCom-0/incplot-lib
