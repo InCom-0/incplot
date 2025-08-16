8491a225a63d9b2b10efc526ee799f6bf08b8341
Michal Lurie <Michal.Lurie@gmail.com>
2025-08-16 23:16:12 +0200

    Proj: Integrated incplot-lib as a git submodule instead of pulling it in as a dependency.

--- CMake_dependencies.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    incplot-lib
-    GIT_REPOSITORY https://github.com/InCom-0/incplot-lib
-    GIT_TAG main
-)
-FetchContent_MakeAvailable(incplot-lib)
