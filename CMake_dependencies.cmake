6ea33a09aed03b252718d304cf3e7d30cbb01391
Michal Lurie <Michal.Lurie@gmail.com>
2026-01-07 14:36:18 +0100

    CMake: Forced static on sqlite3-cmake

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -2,7 +2,10 @@ include(cmake/CPM.cmake)
 
 CPMAddPackage("gh:InCom-0/incplot-lib#main")
 CPMAddPackage("gh:p-ranav/argparse@3.2")
-CPMAddPackage("gh:sjinks/sqlite3-cmake#update-sqlite-3.51.0")
+CPMAddPackage(
+    URI "gh:sjinks/sqlite3-cmake#update-sqlite-3.51.0"
+    OPTIONS "sqlite3_BUILD_SHARED_LIBS OFF"
+)
 
 
 ##############################################################################
