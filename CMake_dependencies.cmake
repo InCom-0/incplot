2fb569007238a55711d5004a9d8411197d4d4dce
Michal Lurie <Michal.Lurie@gmail.com>
2026-01-07 16:24:06 +0100

    CMake: Minor fixes

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -2,6 +2,11 @@ include(cmake/CPM.cmake)
 
 CPMAddPackage("gh:InCom-0/incplot-lib#main")
 CPMAddPackage("gh:p-ranav/argparse@3.2")
+
+if(BUILD_SHARED_LIBS)
+    set(CPM_USE_LOCAL_PACKAGES ON)
+endif()
+
 CPMAddPackage(
     URI "gh:InCom-0/sqlite3-cmake#master"
     NAME SQLite3
