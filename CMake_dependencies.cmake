b3189d909e1d5a070b0a6a63564c6e740f99692a
Michal Lurie <Michal.Lurie@gmail.com>
2026-03-25 01:02:27 +0100

    Version bump to 1.0.1

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,18 +1,11 @@
 include(cmake/CPM_0.42.1.cmake)
 
 
-# CPMAddPackage(
-#     NAME nlohmann_json
-#     URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz
-#     URL_HASH SHA256=42f6e95cad6ec532fd372391373363b62a14af6d771056dbfc86160e6dfff7aa
-#     EXCLUDE_FROM_ALL TRUE
-# )
 CPMAddPackage(
     URI "gh:InCom-0/ots_cmake#cmake_unofficial"
     OPTIONS "ots_BUILD_SHARED_LIB ${BUILD_SHARED_LIBS}"
     NAME ots
 )
-
 CPMAddPackage(
     URI "gh:InCom-0/incplot-lib#main"
     OPTIONS "incplot-lib_BUILD_SHARED_LIB ${BUILD_SHARED_LIBS}"
